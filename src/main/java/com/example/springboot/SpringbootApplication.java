48ad94750354e52c3dffe8b96eac555ea142c625
unknown <markostrauss0703@gmail.com>
2023-11-14 13:39:51 -0500

    add schedule

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -5,15 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
+@EnableScheduling
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
-
+	@Scheduled(fixedDelay = 5000) // Delay in milliseconds (5 min * 60 sec * 1000 ms)
+    public void printHello() {
+        System.out.println("Hello");
+    }
 }
