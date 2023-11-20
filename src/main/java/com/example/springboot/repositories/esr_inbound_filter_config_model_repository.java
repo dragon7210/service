1d28a915ea6578570b641ad83161fdc1f19e1d33
dragon <firedragon7210@gmail.com>
2023-11-20 06:53:32 -0500

    bug fix

--- src/main/java/com/example/springboot/repositories/esr_inbound_filter_config_model_repository.java
+++ src/main/java/com/example/springboot/repositories/esr_inbound_filter_config_model_repository.java
@@ -4,11 +4,7 @@ package com.example.springboot.repositories;
         import org.springframework.data.jpa.repository.JpaRepository;
         import org.springframework.stereotype.Repository;
 
-        import java.util.List;
-        import java.util.Optional;
         import java.util.UUID;
 @Repository
 public interface esr_inbound_filter_config_model_repository extends JpaRepository<esr_inbound_filter_config_model, UUID> {
-//    List<esr_inbound_filter_config_model_repository> findBySent(String sent);
-//    ESR_inbound_filter_model findByUuid(String uuid);
 }
